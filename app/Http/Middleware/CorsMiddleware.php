58824032e855d96f69ac41c05545328324d08d0b
CJ Odina <josephodina@gmail.com>
2019-12-03 19:46:53 +0100

    handle cors errors

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+/**
+ * Intercepts all incoming requests and attaches the
+ * appropriate headers to them in order to guard against
+ * cors errors.
+ */
+class CorsMiddleware
+{
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    $headers = [
+      'Access-Control-Allow-Origin'      => '*',
+      'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Max-Age'           => '86400',
+      'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Accept'
+    ];
+    if ($request->isMethod('OPTIONS'))
+    {
+      return response()->json('{"method":"OPTIONS"}', 200, $headers);
+    }
+    $response = $next($request);
+    foreach($headers as $key => $value)
+    {
+      $response->header($key, $value);
+    }
+    return $response;
+  }
+}
